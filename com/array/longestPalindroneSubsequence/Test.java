0cbc764fa7657e71088d90ae4187b04a2b05adc5
ludwiglevo <ludwiglevo@gmail.com>
2017-10-29 22:33:46 -0400

    Longest palindrome subsequece.

---
+++ com/array/longestPalindroneSubsequence/Test.java
@@ -0,0 +1,13 @@
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "asdzfgfds";
+		Solution sol = new Solution();
+		System.out.println(sol.longestPalindromeSubseq(s));
+
+	}
+
+}
