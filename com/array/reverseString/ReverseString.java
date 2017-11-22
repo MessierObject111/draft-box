f7036e38ea7496d09241b8f7980a671038308175
ludwiglevo <ludwiglevo@gmail.com>
2017-11-21 23:54:31 -0500

    Reverse String

---
+++ com/array/reverseString/ReverseString.java
@@ -0,0 +1,15 @@
+package array.reverseString;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "abcd";
+		String s2 = "";
+		Solution s = new Solution();
+		System.out.println(s.reverseString(s1));
+		System.out.println(s.reverseString(s2));
+
+	}
+
+}
