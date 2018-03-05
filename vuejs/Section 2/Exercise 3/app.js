07c797b485ef76686e50864cc33c65ae37151d82
ludwiglevo <ludwiglevo@gmail.com>
2018-03-05 23:00:24 +0800

    Exercise 3

--- vuejs/Section 2/Exercise 3/app.js
+++ vuejs/Section 2/Exercise 3/app.js
@@ -2,5 +2,26 @@ new Vue({
         el: '#exercise',
         data: {
             value: 0
+        },
+        computed: {
+        	result: function() {
+          	return this.value <= 37 ? 'Not there yet' : 'Done';
+          },
+          now: function () {
+            return Date.now()
+          }
+        },
+        watch: {
+        	value: function(){
+          	var self = this;
+          	setTimeout(function(){
+            	self.value = 0;
+            }, 2000);
+          }
+        },
+        methods: {
+          nowMethod: function () {
+            return Date.now()
+          }
         }
     });
