338d4d7bf2124118cda552aea6ed3ac852dd4103
ludwiglevo <ludwiglevo@gmail.com>
2018-05-02 16:16:07 +0800

    Clean up code leftover

--- javascipt-core/closure/multi-layered-functions.js
+++ javascipt-core/closure/multi-layered-functions.js
@@ -5,34 +5,13 @@ function layer1(x){
     }
   }
 }
-layer1(1)(2)(3);// x:1 y:2 z:3
+layer1(1)(2)(3);
+//Output: x:1 y:2 z:3
 
 /* It equals to the following:*/
 var layer2_ref = layer1(1);
 
 var layer3_ref = layer2_ref(2);
 
-layer3_ref(3);//x:1 y:2 z:3
-
-
-//Module Pattern
-function Counter(){
-  let i = 0;
-  function add(){
-    return ++i;
-  }
-  function minus(){
-    return --i;
-  }
-  return {
-    add: function(){
-      console.log(i);
-      add();
-    },
-    minus: function(){
-      minus();
-    },
-    value: i
-  }
-}
-var counter = Counter();
+layer3_ref(3);
+//Output: x:1 y:2 z:3
